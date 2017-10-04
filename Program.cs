96baae81816bb74babb179c2e387c7082a16e416
MsAlexis3 <atchison06@gmail.com>
2017-10-04 13:32:15 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Net.Mail;
+
+namespace weekly_status_update
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+
+            var path = appSettings["TemplatePath"];
+            object outputPath = appSettings["WordOutputPath"];
+            object oMissing = System.Reflection.Missing.Value;
+            System.Globalization.CultureInfo ci = System.Threading.Thread.CurrentThread.CurrentCulture;
+            DayOfWeek fdow = ci.DateTimeFormat.FirstDayOfWeek;
+            DayOfWeek today = DateTime.Now.DayOfWeek;
+            DateTime sow = DateTime.Now.AddDays(-(today - fdow)).Date;
+            DateTime eow = sow.AddDays(6);
+            DateTime friday = eow.AddDays(-1);
+            const string emailSmtpHost = "SMTPR.illinois.gov";
+            const string emailSendFrom = "alexis.atchison@illinois.gov";
+            const string emailSendFromDisplay = "Atchison, Alexis";
+            string outputFileName;
+
+            var byteArray = File.ReadAllBytes(path);
+            using (MemoryStream stream = new MemoryStream())
+            {
+                stream.Write(byteArray, 0, (int)byteArray.Length);
+                using (WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(stream, true))
+                {
+                    var document = wordprocessingDocument.MainDocumentPart.Document;
+
+                    foreach (var text in document.Descendants<Text>())
+                        switch (text.Text)
+                        {
+                            case "{Todays Date}":
+                                text.Text = text.Text.Replace("{Todays Date}", friday.ToString("MMMM dd, yyyy"));
+                                break;
+                            case "{Reporting Period}":
+                                text.Text = text.Text.Replace("{Reporting Period}", $"{sow:MM/dd/yyyy} - {eow:MM/dd/yyyy}");
+                                break;
+                            case "{Next Report Date}":
+                                text.Text = text.Text.Replace("{Next Report Date}", friday.AddDays(7).ToString("MMMM dd, yyyy"));
+                                break;
+                            case "{Consultant Name}":
+                                text.Text = text.Text.Replace("{Consultant Name}", appSettings["ConsultantName"]);
+                                break;
+                        }
+
+                    outputFileName = outputPath.ToString().Replace(".docx", $" {sow:MM.dd.yyyy}.docx");
+                    wordprocessingDocument.Close();
+                    wordprocessingDocument.Dispose();
+                }
+
+                using (FileStream fs = new FileStream(outputFileName, FileMode.OpenOrCreate))
+                {
+                    stream.WriteTo(fs);
+                    fs.Close();
+                    fs.Dispose();
+                }
+                stream.Close();
+                stream.Dispose();
+            }
+            
+            var emailSendTo = appSettings["EmailTo"];
+#if DEBUG
+                emailSendTo = "alexis.atchison@illinois.gov";
+#endif
+            var emailSubject = appSettings["EmailSubject"];
+            var client = new SmtpClient(emailSmtpHost);
+
+            using (MailMessage message = new MailMessage())
+            {
+                message.IsBodyHtml = true;
+                message.BodyEncoding = System.Text.Encoding.UTF8;
+                message.Subject = emailSubject;
+                message.SubjectEncoding = System.Text.Encoding.UTF8;
+                message.Bcc.Add(new MailAddress(emailSendFrom));
+                message.From = new MailAddress(emailSendFrom, emailSendFromDisplay, System.Text.Encoding.UTF8);
+                foreach (var x in emailSendTo.Split(','))
+                {
+                    message.To.Add(new MailAddress(x));
+                }
+            message.Attachments.Add(new Attachment(outputFileName.ToString()));
+            message.Body = $"{message.Body}<span style='font-size:11pt;font-family:Calibri'>{appSettings["EmailBody"]}</span>";
+
+            client.Send(message);
+
+            message.Dispose();
+        }
+    }
+}
+}
